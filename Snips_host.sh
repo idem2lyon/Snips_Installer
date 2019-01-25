7cd151f4e86d381d1e6a19c58aaac9a699f97d3d
joe-achim <swen@kerric.fr>
2019-01-25 14:17:06 +0100

    Update Snips_host.sh

--- Snips_host.sh
+++ Snips_host.sh
@@ -10,7 +10,13 @@ cd ~/
 
 
 #Install Snips Plateform 
-sudo apt-get install snips-platform-voice
+sudo apt-get update
+sudo apt-get install -y dirmngr
+sudo bash -c  'echo "deb   https://raspbian.snips.ai/$(lsb_release -cs) stable main" > /etc/apt/sources.list.d/snips.list'
+sudo apt-key adv --keyserver pgp.mit.edu --recv-keys D4F50CDCA10A2849
+sudo apt-get update
+sudo apt-get install -y snips-platform-voice
+sudo apt-get install -y snips-watch
 
 #Prepare for add satellites https://github.com/Psychokiller1888/satConnect
 cd ~/
