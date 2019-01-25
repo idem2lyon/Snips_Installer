f76342465323be93a57fac3e51f547a6f26bdae3
joe-achim <swen@kerric.fr>
2019-01-25 14:19:50 +0100

    Update Snips_sat.sh

--- Snips_sat.sh
+++ Snips_sat.sh
@@ -1,13 +1,3 @@
-# Install NPM
-curl -L https://npmjs.com/install.sh | sudo sh
-
-# Install Sound-Card & Drivers from : https://github.com/Psychokiller1888/snipsLedControl/
-cd ~/
-wget https://gist.githubusercontent.com/Psychokiller1888/a9826f92c5a3c5d03f34d182fda1ce4c/raw/e24882e8997730dcf7a308e303b3b88001dbbfa1/slc_download.sh
-sudo chmod +x slc_download.sh
-sudo ./slc_download.sh
-cd ~/
-
 
 #Install Snips Plateform 
 sudo apt-get update
@@ -18,11 +8,20 @@ sudo apt-get update
 sudo apt-get install -y snips-audio-server
 sudo apt-get install -y snips-watch
 
+# Install NPM
+curl -L https://npmjs.com/install.sh | sudo sh
+sudo npm install -g snips-sam
+
 #Prepare for add satellites https://github.com/Psychokiller1888/satConnect
 cd ~/
 git clone https://github.com/Psychokiller1888/satConnect.git
 cd satConnect
-sudo python connect.py
+# sudo python connect.py
 cd ~/
 
-
+# Install Sound-Card & Drivers from : https://github.com/Psychokiller1888/snipsLedControl/
+cd ~/
+wget https://gist.githubusercontent.com/Psychokiller1888/a9826f92c5a3c5d03f34d182fda1ce4c/raw/e24882e8997730dcf7a308e303b3b88001dbbfa1/slc_download.sh
+sudo chmod +x slc_download.sh
+sudo ./slc_download.sh
+cd ~/
