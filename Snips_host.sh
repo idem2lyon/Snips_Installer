bc8f9a9133c4c085241dd0911432b1e4e4c4f087
joe-achim <swen@kerric.fr>
2019-01-26 00:05:53 +0100

    Update Snips_host.sh

--- Snips_host.sh
+++ Snips_host.sh
@@ -10,6 +10,7 @@ sudo apt-get install lsb-release
 # Install Snips Plateform 
 sudo bash -c  'echo "deb   https://raspbian.snips.ai/$(lsb_release -cs) stable main" > /etc/apt/sources.list.d/snips.list'
 sudo apt-key adv --keyserver pgp.mit.edu --recv-keys D4F50CDCA10A2849
+sudo apt-key adv --keyserver pgp.surfnet.nl --recv-keys D4F50CDCA10A2849
 sudo apt-get update
 sudo apt-get install -y snips-platform-voice
 sudo apt-get install -y snips-watch
@@ -37,6 +38,7 @@ sudo apt-get update
 sudo apt-get install -y dirmngr apt-transport-https
 sudo bash -c  'echo "deb https://debian.snips.ai/stretch stable main" > /etc/apt/sources.list.d/snips.list'
 sudo apt-key adv --keyserver pgp.mit.edu --recv-keys F727C778CCB0A455
+sudo apt-key adv --keyserver pgp.surfnet.nl --recv-keys F727C778CCB0A455
 sudo npm install -g snips-sam
 sam connect Salon
 sam init
