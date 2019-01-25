d6b2c8fa9955f9d3549645267eba8119db7d20ba
joe-achim <swen@kerric.fr>
2019-01-25 17:43:49 +0100

    Update Snips_sat.sh

--- Snips_sat.sh
+++ Snips_sat.sh
@@ -1,7 +1,9 @@
 
-#Install Snips Plateform 
+#Install Snips Audio Server 
 sudo apt-get update
 sudo apt-get install -y dirmngr
+sudo apt-cache policy lsb-release
+sudo apt-get install lsb-release
 sudo bash -c  'echo "deb   https://raspbian.snips.ai/$(lsb_release -cs) stable main" > /etc/apt/sources.list.d/snips.list'
 sudo apt-key adv --keyserver pgp.mit.edu --recv-keys D4F50CDCA10A2849
 sudo apt-get update
