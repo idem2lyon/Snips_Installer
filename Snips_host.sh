c395eddeefd356e457ee040909eeff311fc396e9
joe-achim <swen@kerric.fr>
2019-01-25 17:43:00 +0100

    Update Snips_host.sh

--- Snips_host.sh
+++ Snips_host.sh
@@ -1,6 +1,8 @@
 #Install Snips Plateform 
 sudo apt-get update
 sudo apt-get install -y dirmngr
+sudo apt-cache policy lsb-release
+sudo apt-get install lsb-release
 sudo bash -c  'echo "deb   https://raspbian.snips.ai/$(lsb_release -cs) stable main" > /etc/apt/sources.list.d/snips.list'
 sudo apt-key adv --keyserver pgp.mit.edu --recv-keys D4F50CDCA10A2849
 sudo apt-get update
